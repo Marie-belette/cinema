75e9d6a93adf53c001a8743f56937b6eec06ea47
Belette <marie.maillos@gmail.com>
2020-01-24 14:13:44 +0100

    add person service interface

--- src/main/java/cinema/controller/PersonController.java
+++ src/main/java/cinema/controller/PersonController.java
@@ -2,11 +2,16 @@ package cinema.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import cinema.persistence.entity.Person;
+import cinema.service.IPersonService;
 
 public class PersonController {
+	
+	@Autowired
+	IPersonService personService;
 	
 	@GetMapping
 	List<Person>  getAllPersons() {
@@ -18,7 +23,7 @@ public class PersonController {
 	Person addPerson(Person person) {
 	// TODO
 	return person;
-}
+	}
 
 
 }
